833d81db3068c470ef3a8d851d7a60b99f83b083
Chris Maciejewski <chris@level7systems.co.uk>
2015-10-22 17:31:00 +0100

    Allow any origin

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(morgan('combined'))
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:1841');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', req.header('Access-Control-Request-Headers'));
 
