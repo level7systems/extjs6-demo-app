c75b2930e4a264d39ebe85a549b6b4b6d51e0f8d
Chris Maciejewski <chris@level7systems.co.uk>
2015-10-22 22:17:35 +0100

    when Grid component is created store instance is not attached yet, so need to wait a bit for the actual instance binding

--- app/overrides/grid/SizeAwarePanel.js
+++ app/overrides/grid/SizeAwarePanel.js
@@ -15,7 +15,25 @@ Ext.define('MyApp.override.grid.SizeAwarePanel', {
         Ext.log('MyApp.override.grid.SizeAwarePanel onResize, view height: ' + view.getHeight() + ', setting pageSize to ' + pageSize);
         
         var store = this.getStore();
-        store.setPageSize(pageSize);
-        store.loadPage(1);
+        
+        store.on('beforeload', function(store) {
+            Ext.log('MyApp.override.grid.SizeAwarePanel store.beforeload');
+        });
+        
+        /*
+        console.log(store.storeId);
+        if (store.storeId == 'ext-empty-store') {
+            console.log('we are here 1');
+            // wait till we bind the actual store instance
+            Ext.Function.defer(function(){
+                console.log('we are here 3');
+                store.setPageSize(pageSize);
+                store.loadPage(1);
+            }, 1000);
+        } else {
+            console.log('we are here 2');
+            store.setPageSize(pageSize);
+            store.loadPage(1);            
+        }*/
     }
 });
